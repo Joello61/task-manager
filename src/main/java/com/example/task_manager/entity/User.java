81223fef5be80bf38fd12ff75b3556076ce4bd98
Joello61 <tchindajoel61@gmail.com>
2026-01-10 15:12:48 +0100

    Add User entity and Lombok dependency to project

---
+++ src/main/java/com/example/task_manager/entity/User.java
@@ -0,0 +1,34 @@
+package com.example.task_manager.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role;
+
+    @Column(nullable = false)
+    private Instant dateCreation;
+
+}
