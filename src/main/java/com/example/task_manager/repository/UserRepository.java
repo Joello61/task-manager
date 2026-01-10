517d02af3c9164505ea10613607f325d93f7d3fd
Joello61 <tchindajoel61@gmail.com>
2026-01-10 15:19:56 +0100

    Refactor User entity and enhance repository functionality
    - Switch `@GeneratedValue` strategy to `IDENTITY` in `User` entity.
    - Add `@NoArgsConstructor` and `@AllArgsConstructor` annotations to `User`.
    - Implement `@PrePersist` method to initialize `dateCreation`.
    - Replace `Repository` with `JpaRepository` in `UserRepository`.
    - Update repository to include `findByEmail` method.

--- src/main/java/com/example/task_manager/repository/UserRepository.java
+++ src/main/java/com/example/task_manager/repository/UserRepository.java
@@ -1,12 +1,12 @@
 package com.example.task_manager.repository;
 
 import com.example.task_manager.entity.User;
-import org.springframework.data.repository.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface UserRepository extends Repository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
-    Optional<User> findById(Long id);
+    Optional<User> findByEmail (String email);
 
 }
