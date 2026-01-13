cefed8d1d8265f2a9f4466a6c7b3fc8c1a4a4ff6
Joello61 <tchindajoel61@gmail.com>
2026-01-13 11:29:37 +0100

    Add TaskService for task management operations
    - Implement CRUD methods: save, findAll, findById, findByUser, update, and delete.
    - Integrate TaskRepository, UserRepository, and TaskMapper.
    - Add exception handling for missing tasks and users.

---
+++ src/main/java/com/example/task_manager/service/TaskService.java
@@ -0,0 +1,71 @@
+package com.example.task_manager.service;
+
+import com.example.task_manager.dto.task.CreateTaskDto;
+import com.example.task_manager.dto.task.TaskResponseDto;
+import com.example.task_manager.entity.Task;
+import com.example.task_manager.entity.User;
+import com.example.task_manager.exception.TaskNotFoundException;
+import com.example.task_manager.exception.UserNotFoundException;
+import com.example.task_manager.mapper.TaskMapper;
+import com.example.task_manager.repository.TaskRepository;
+import com.example.task_manager.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TaskService {
+
+    private final TaskRepository taskRepository;
+    private final UserRepository userRepository;
+    private final TaskMapper taskMapper;
+
+    public TaskService(TaskRepository taskRepository, UserRepository userRepository, TaskMapper taskMapper) {
+        this.taskRepository = taskRepository;
+        this.userRepository = userRepository;
+        this.taskMapper = taskMapper;
+    }
+
+    public TaskResponseDto save(CreateTaskDto taskDto) {
+        User user = userRepository.findById(taskDto.getUserId()).orElseThrow(() -> new UserNotFoundException(taskDto.getUserId()));
+
+        Task task = taskMapper.toEntity(taskDto);
+        task.setUser(user);
+        Task taskSave = taskRepository.save(task);
+
+        return taskMapper.toResponseDto(taskSave);
+    }
+
+    public List<TaskResponseDto> findAll() {
+        return taskRepository.findAll().stream().map(taskMapper::toResponseDto).toList();
+    }
+
+    public TaskResponseDto findById(Long taskId) {
+        return taskRepository.findById(taskId).map(taskMapper::toResponseDto).orElseThrow(() -> new TaskNotFoundException(taskId));
+    }
+
+    public List<TaskResponseDto> findByUser(Long idUser) {
+        User user = userRepository.findById(idUser).orElseThrow(() -> new UserNotFoundException(idUser));
+        return taskRepository.findAllByUser(user).stream().map(taskMapper::toResponseDto).toList();
+    }
+
+    public TaskResponseDto update(Long taskId, CreateTaskDto taskDto) {
+        Task task = taskRepository.findById(taskId).orElseThrow(() -> new TaskNotFoundException(taskId));
+        User user = userRepository.findById(taskDto.getUserId()).orElseThrow(() -> new UserNotFoundException(taskDto.getUserId()));
+
+        task.setTitle(taskDto.getTitle());
+        task.setDescription(taskDto.getDescription());
+        task.setDone(taskDto.isDone());
+        task.setUser(user);
+
+        Task taskUpdated = taskRepository.save(task);
+        return taskMapper.toResponseDto(taskUpdated);
+    }
+
+    public void delete(Long id) {
+        if (!taskRepository.existsById(id)) {
+            throw new TaskNotFoundException(id);
+        }
+        taskRepository.deleteById(id);
+    }
+}
