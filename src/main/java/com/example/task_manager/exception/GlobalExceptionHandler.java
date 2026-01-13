49b830c345fa56a37d5e5262a87b083b476129cd
Joello61 <tchindajoel61@gmail.com>
2026-01-13 11:29:14 +0100

    Add TaskNotFoundException and global exception handling
    - Create TaskNotFoundException for missing task scenarios.
    - Handle TaskNotFoundException in GlobalExceptionHandler.
    - Return standardized ApiResponse with HTTP 404 status.

--- src/main/java/com/example/task_manager/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/task_manager/exception/GlobalExceptionHandler.java
@@ -19,6 +19,11 @@ public class GlobalExceptionHandler {
         return ApiResponseBuilder.error(ex.getMessage(), HttpStatus.NOT_FOUND, null);
     }
 
+    @ExceptionHandler
+    public ResponseEntity<ApiResponse<String>> handleTaskNotFoundException(TaskNotFoundException ex) {
+        return ApiResponseBuilder.error(ex.getMessage(), HttpStatus.NOT_FOUND, null);
+    }
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ApiResponse<Map<String, String>>> handleValidationErrors(MethodArgumentNotValidException ex) {
         Map<String, String> errors = new HashMap<>();
