2d87a912a83fa501b856c450084a8b521705059d
Joello61 <tchindajoel61@gmail.com>
2026-01-17 15:59:24 +0100

    Refactor codebase for consistency and introduce project-wide Checkstyle
    - Format code and function declarations for improved readability and clarity.
    - Add `checkstyle.xml` configuration file to maintain a standardized code style.
    - Update `pom.xml` to utilize variables for dependency and plugin versions.
    - Enhance Maven build with Checkstyle plugin for code validation.

--- src/main/java/com/example/task_manager/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/task_manager/exception/GlobalExceptionHandler.java
@@ -16,36 +16,43 @@ import java.util.Map;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler
-    public ResponseEntity<ApiResponse<String>> handleUserNotFoundException(UserNotFoundException ex) {
+    public ResponseEntity<ApiResponse<String>> handleUserNotFoundException(
+            final UserNotFoundException ex) {
         return ApiResponseBuilder.error(ex.getMessage(), HttpStatus.NOT_FOUND, null);
     }
 
     @ExceptionHandler
-    public ResponseEntity<ApiResponse<String>> handleTaskNotFoundException(TaskNotFoundException ex) {
+    public ResponseEntity<ApiResponse<String>> handleTaskNotFoundException(
+            final TaskNotFoundException ex) {
         return ApiResponseBuilder.error(ex.getMessage(), HttpStatus.NOT_FOUND, null);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ApiResponse<Map<String, String>>> handleValidationErrors(MethodArgumentNotValidException ex) {
+    public ResponseEntity<ApiResponse<Map<String, String>>> handleValidationErrors(
+            final MethodArgumentNotValidException ex) {
         Map<String, String> errors = new HashMap<>();
 
-        ex.getBindingResult().getFieldErrors().forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
+        ex.getBindingResult().getFieldErrors()
+                .forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
 
         return ApiResponseBuilder.error("Validation failed", HttpStatus.BAD_REQUEST, errors);
     }
 
     @ExceptionHandler
-    public ResponseEntity<ApiResponse<String>> handleUserAlreadyExistException(UserAlreadyExistException ex) {
+    public ResponseEntity<ApiResponse<String>> handleUserAlreadyExistException(
+            final UserAlreadyExistException ex) {
         return ApiResponseBuilder.error(ex.getMessage(), HttpStatus.BAD_REQUEST, null);
     }
 
     @ExceptionHandler
-    public ResponseEntity<ApiResponse<String>> handleTaskAlreadyExistException(TaskAlreadyExistException ex) {
+    public ResponseEntity<ApiResponse<String>> handleTaskAlreadyExistException(
+            final TaskAlreadyExistException ex) {
         return ApiResponseBuilder.error(ex.getMessage(), HttpStatus.BAD_REQUEST, null);
     }
 
     @ExceptionHandler(ConstraintViolationException.class)
-    public ResponseEntity<ApiResponse<String>> handleConstraintViolationException(ConstraintViolationException ex) {
+    public ResponseEntity<ApiResponse<String>> handleConstraintViolationException(
+            final ConstraintViolationException ex) {
         return ApiResponseBuilder.error(ex.getMessage(), HttpStatus.BAD_REQUEST, null);
     }
 
