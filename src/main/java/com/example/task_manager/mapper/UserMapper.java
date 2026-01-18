bb573f771023481c8956a5137916d5fcfac5d858
Joello61 <tchindajoel61@gmail.com>
2026-01-18 20:36:02 +0100

    Map `RegisterDto` to `CreateUserDto` with default role mapping

--- src/main/java/com/example/task_manager/mapper/UserMapper.java
+++ src/main/java/com/example/task_manager/mapper/UserMapper.java
@@ -1,12 +1,17 @@
 package com.example.task_manager.mapper;
 
+import com.example.task_manager.dto.auth.RegisterDto;
 import com.example.task_manager.dto.user.CreateUserDto;
 import com.example.task_manager.dto.user.UserResponseDto;
 import com.example.task_manager.entity.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
     UserResponseDto toResponseDto(User user);
     User toEntity(CreateUserDto userDto);
+
+    @Mapping(target = "role", constant = "USER")
+    CreateUserDto toCreateDto(RegisterDto registerDto);
 }
