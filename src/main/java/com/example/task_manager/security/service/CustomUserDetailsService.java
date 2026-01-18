a96702f38a442628d0b8b7c340c55668d4a74d0a
Joello61 <tchindajoel61@gmail.com>
2026-01-18 20:34:54 +0100

    Add `CustomUserDetailsService` for loading user details by email
    - Implement `UserDetailsService` to integrate with the `UserRepository` for user retrieval.
    - Add exception handling for missing users and return custom `UserDetailsImpl` instances.

---
+++ src/main/java/com/example/task_manager/security/service/CustomUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.example.task_manager.security.service;
+
+import com.example.task_manager.entity.User;
+import com.example.task_manager.repository.UserRepository;
+import com.example.task_manager.security.UserDetailsImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    @Transactional(readOnly = true)
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Utilisateur non trouvé : " + email));
+        return new UserDetailsImpl(user);
+    }
+}
