691892e63bc42bce0fde2aa90f041382f38d6714
Joello61 <tchindajoel61@gmail.com>
2026-01-19 15:51:02 +0100

    Update `findAllByUser` in `TaskRepository` to support pagination with `Pageable`

--- src/main/java/com/example/task_manager/repository/TaskRepository.java
+++ src/main/java/com/example/task_manager/repository/TaskRepository.java
@@ -2,6 +2,8 @@ package com.example.task_manager.repository;
 
 import com.example.task_manager.entity.Task;
 import com.example.task_manager.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,6 +12,6 @@ import java.util.Optional;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
-    List<Task> findAllByUser(User user);
+    Page<Task> findAllByUser(User user, Pageable pageable);
     Optional<Task> findByTitle(String title);
 }
